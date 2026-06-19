4b608178dd98f3849e3a84d05048ffda24784be8
Luiz Oliveira <felipeluiz75@outlook.com>
2026-06-19 17:54:10 -0300

    feat: add databaseurl neon

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import dj_database_url
 load_dotenv()
 """
 Django settings for config project.
@@ -89,14 +90,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
